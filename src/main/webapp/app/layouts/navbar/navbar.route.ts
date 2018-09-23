e2131bf4b4ed79e3e96195ae1799a78885b7419f
jzheadley <headleyjz@vcu.edu>
2018-09-23 06:44:36 -0400

    added the routing for the extra pages

--- src/main/webapp/app/layouts/navbar/navbar.route.ts
+++ src/main/webapp/app/layouts/navbar/navbar.route.ts
@@ -1,9 +1,27 @@
 import { Route } from '@angular/router';
 
 import { NavbarComponent } from './navbar.component';
+import { TransportComponent } from 'app/layouts/transport/transport.component';
+import { LegalCounselComponent } from 'app/layouts/legal-counsel/legal-counsel.component';
+import { FinanceComponent } from 'app/layouts/finance/finance.component';
 
 export const navbarRoute: Route = {
     path: '',
     component: NavbarComponent,
     outlet: 'navbar'
+};
+
+export const transportRoute: Route = {
+    path: 'transport',
+    component: TransportComponent
+};
+
+export const legalCounselRoute: Route = {
+    path: 'legal-counsel',
+    component: LegalCounselComponent
+};
+
+export const FinanceRoute: Route = {
+    path: 'finance',
+    component: FinanceComponent
 };
