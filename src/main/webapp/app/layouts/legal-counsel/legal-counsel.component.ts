f713b6b8ccf238ea1c2d6a4a39611a2d9dc75c1c
abbydoh <aedohmeier@gmail.com>
2018-09-23 09:03:02 -0400

    further scaffolding, all pages scaffolded

--- src/main/webapp/app/layouts/legal-counsel/legal-counsel.component.ts
+++ src/main/webapp/app/layouts/legal-counsel/legal-counsel.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'jhi-legal-counsel',
-    templateUrl: './legal-counsel.component.html',
-    styles: []
+    templateUrl: 'legal-counsel.component.html',
+    styleUrls: ['legal-counsel.component.scss']
 })
 export class LegalCounselComponent implements OnInit {
     constructor() {}
