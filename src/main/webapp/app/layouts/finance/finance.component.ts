d350f0f6280ab7806e8fa48db21a9b1446ce8db9
jzheadley <headleyjz@vcu.edu>
2018-09-23 06:47:11 -0400

    forgot these files

---
+++ src/main/webapp/app/layouts/finance/finance.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-finance',
+    templateUrl: './finance.component.html',
+    styles: []
+})
+export class FinanceComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
---
+++ src/main/webapp/app/layouts/transport/transport.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-transport',
+    templateUrl: './transport.component.html',
+    styles: []
+})
+export class TransportComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
